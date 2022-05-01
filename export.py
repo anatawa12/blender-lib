067a60ad48572211d1ea72613808c1ca007e23fa
anatawa12 <anatawa12@icloud.com>
2022-05-01 11:41:42 +0900

    select all vertices before remove_doubles

--- export.py
+++ export.py
@@ -121,6 +121,7 @@ def freeze_modifiers(obj: Object, modifiers: typing.Iterable[str]):
 def merge_by_distance(obj: Object, threshold=0.0001):
     select_objects([obj])
     bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.select_all(action='SELECT')
     bpy.ops.mesh.remove_doubles(threshold=threshold)
     bpy.ops.object.mode_set(mode='OBJECT')
 
